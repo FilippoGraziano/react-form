f54630be872ed33a7f890998c2a71e8f1865f833
Filippo Graziano <filippograziano13@gmail.com>
2026-07-16 16:28:10 +0200

    add style to form modify grid template of articles list in flex

--- src/components/components-jsx/Form.jsx
+++ src/components/components-jsx/Form.jsx
@@ -6,33 +6,38 @@ const Form = props => {
     const [input, setInput] = useState({ title: ``, description: `` })
 
     return (
-    
-    <form onSubmit={e => (
 
-        e.preventDefault(),
+        <form onSubmit={e => (
 
-        props.addArticles([...props.articles, { ...props.articles.at(-1), title: input.title, id: (props.articles.at(-1).id + 1), description: input.description }]),
+            e.preventDefault(),
 
-        setInput({ title: ``, description: `` })
+            props.addArticles([...props.articles, { ...props.articles.at(-1), title: input.title, id: (props.articles.at(-1).id + 1), description: input.description }]),
 
-    )}>
+            setInput({ title: ``, description: `` })
 
-        <div>
-            <label id='new-title'>Scrivi un titolo</label>
-            <input type="text" value={input.title} onChange={e => setInput({ ...input, title: e.target.value })} id='new-title' />
-        </div>
+        )}>
+            <div id="inputs">
 
-        <div>
+                <div>
 
-            <label id='new-description'>Aggiungi una descrizione</label>
-            <input type="text" value={input.description} onChange={e => setInput({ ...input, description: e.target.value })} />
+                    <label id='new-title'>Scrivi un titolo</label>
+                    <input type="text" placeholder="Aggiungi titolo" value={input.title} onChange={e => setInput({ ...input, title: e.target.value })} />
 
-        </div>
+                </div>
 
-        <button>Aggiungi</button>
+                <div>
 
-    </form>
-    
+                    <label id='new-description'>Aggiungi una descrizione</label>
+                    <input type="text" placeholder="Aggiungi descrizione" value={input.description} onChange={e => setInput({ ...input, description: e.target.value })} />
+
+                </div>
+
+            </div>
+
+            <button>Aggiungi</button>
+
+        </form>
+
     );
 
 };
