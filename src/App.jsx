01b47cf09d3c083d35400f1ff8f416f6f55d33e2
Filippo Graziano <filippograziano13@gmail.com>
2026-07-16 14:51:50 +0200

    add list of articles add form that add new articles title and description

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,49 @@ import { articles } from './components/data'
 
 const App = () => {
 
+  const [input, setInput] = useState({title: ``, description: ``})
+  const [_articles, setArticles] = useState(articles)
+
   return (
     <>
+
+      <ul>
+
+        {_articles.map(el => (
+
+          <li key={el.id}>
+            <h2>{el.title}</h2>
+          </li>
+
+        ))}
+
+      </ul>
+
+      <form onSubmit={e => (
+
+        e.preventDefault(),
+
+        setArticles([...articles, {..._articles.at(-1), title: input.title, id: (_articles.at(-1).id + 1), description: input.description}]),
+
+        setInput({title: ``, description: ``})
+
+      )}>
+
+        <div>
+          <label id='new-title'>Scrivi un titolo</label>
+          <input type="text" value={input.title} onChange={e => setInput({...input, title: e.target.value})} id='new-title' />
+        </div>
+
+        <div>
+
+          <label id='new-description'>Aggiungi una descrizione</label>
+          <input type="text" value={input.description} onChange={e => setInput({...input, description: e.target.value})} />
+
+        </div>
+
+        <button>Aggiungi</button>
+
+      </form>
 
     </>
   )
