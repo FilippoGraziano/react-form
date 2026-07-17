6306cbeaeb303bd680d77af0b0cf33bbb5c37051
Filippo Graziano <filippograziano13@gmail.com>
2026-07-17 13:36:54 +0200

    modify style of articles list

--- src/components/components-jsx/ArticlesList.jsx
+++ src/components/components-jsx/ArticlesList.jsx
@@ -8,9 +8,9 @@ const ArticlesList = props => (
 
             <li key={el.id}>
                 <h2>{el.title}</h2>
+                <address>{el.author}</address>
                 <p>{el.body}</p>
-                <span>Autore: {el.author}</span>
-                <span>{el.public? `Questo articolo è publico` : `Questo articolo è privato`}</span>
+                <span id="state">{el.public? `Publico!` : `Privato!`}</span>
                 <button onClick={() => props.removeArticles(props.articles.filter(newEl => newEl.id !== el.id))}>
                     <i className="bi bi-trash3"></i>
                 </button>
